e62a46e570d6c413d5917712858cf26582a05606
Mahir Kücük <mahirkucuk@outlook.com>
2023-09-04 20:23:18 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Board from '@/components/Board'
+import Header from '@/components/Header'
+import React from 'react'
+
+export default function page() {
+  return (
+    <main>
+      {/*Header*/}
+      <Header/>
+      {/*Body*/}
+      <Board/>
+    </main>
+  )
+}
+
