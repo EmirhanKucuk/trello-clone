456b8e2e49415200db0bd8743a0be331ad3737f3
Mahir Kücük <mahirkucuk@outlook.com>
2023-09-04 23:17:27 +0200

    sorted the columns

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,15 +1,17 @@
+import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
 import { create } from 'zustand'
 
 interface BoardState {
-    board:Board;
-    getBoard: ()=> void;
+    board: Board;
+    getBoard: () => void;
 }
 
-const useBoardStore = create<BoardState>((set)=> ({
-    board:{
-        columns: new Map<TypedColumn,Column>()
+export const useBoardStore = create<BoardState>((set) => ({
+    board: {
+        columns: new Map<TypedColumn, Column>()
     },
-    getBoard: () =>{
-
+    getBoard: async () => {
+        const board = await getTodosGroupedByColumn();
+        set({ board });
     }
 }))
