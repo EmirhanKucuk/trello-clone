b2596cac5b1ed442e2ac648a4d8917f09ef708ad
Mahir Kücük <mahirkucuk@outlook.com>
2023-09-17 18:37:28 +0200

    trying openai

--- lib/fetchSuggestion.ts
+++ lib/fetchSuggestion.ts
@@ -1,11 +1,14 @@
+
+
 const fetchSuggestion = async (board: Board) => {
     const todos = formatTodosForAI(board);
-
+    console.log(todos)
     const res = await fetch("/api/generateSummary", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
+
         body: JSON.stringify({ board })
     });
     const GPTdata = await res.json();
--- openai.ts
+++ openai.ts
@@ -1,7 +1,10 @@
 import { Configuration, OpenAIApi } from "openai";
+
 const configuration = new Configuration({
+    organization: "org-ZhLUXCBdmgEuHkCV1LMB8oPI",
     apiKey: process.env.OPENAI_API_KEY,
 });
+console.log(configuration)
 const openai = new OpenAIApi(configuration);
 
-export default openai
+export default openai;
